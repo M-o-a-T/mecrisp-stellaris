97017d385e6a11f36f0ded1316a0a6e277c04f90
Matthias Urlichs <matthias@urlichs.de>
2021-10-11 13:22:55 +0200

    Teach the assembler to include an "option.s" file
    possibly from ../common/

--- mecrisp-stellaris-source/common/datastackandmacros.s
+++ mecrisp-stellaris-source/common/datastackandmacros.s
@@ -16,6 +16,9 @@
 @    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 @
 
+@ macros
+.include "options.s"
+
 @ -----------------------------------------------------------------------------
 @ Bit-position equates (for setting or clearing a single bit)
 @ -----------------------------------------------------------------------------
